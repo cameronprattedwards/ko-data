ff4bd6f3099cc2da21c8a101d55304fb981bf038
Cameron Edwards <cameronprattedwards@gmail.com>
2014-06-25 11:45:45 -0400

    Add Number type

---
+++ type/Number.js
@@ -0,0 +1,18 @@
+define(["knockout", "./Type.js"], function (ko, Type) {
+	var Number = Type.extend({
+		value: 0,
+		parse: function (input) {
+			return parseFloat(input);
+		}
+	});
+
+	var output = function (options) {
+		return new Number(options);
+	}
+
+	output.value = 0;
+	output.getInstance = Number.prototype.getInstance;
+	output.parse = Number.prototype.parse;
+
+	return output;
+});
---
+++ type/spec/Number-spec.js
@@ -0,0 +1,34 @@
+define(["../Number.js", "knockout"], function (Number, ko) {
+	describe("Number", function () {
+		var numType = Number;
+
+		describe("#getInstance", function () {
+			it("is a function", function () {
+				expect(typeof numType.getInstance).toBe("function");
+			});
+
+			it("returns an observable number", function () {
+				var instance = numType.getInstance();
+				expect(ko.isObservable(instance)).toBe(true);
+				expect(typeof instance()).toBe("number");
+			});
+
+			it("defaults to 0", function () {
+				expect(ko.utils.unwrapObservable(numType.getInstance())).toBe(0);
+			});
+		});
+
+		it("is configurable", function () {
+			expect(function () { Number() }).not.toThrow();
+			var configged = Number({ value: 9 });
+			expect(ko.utils.unwrapObservable(configged.getInstance())).toBe(9);
+		});
+
+		describe("#parse", function () {
+			it("turns strings into numbers", function () {
+				expect(numType.parse("17")).toBe(17);
+			});		
+		});
+	});
+
+});
