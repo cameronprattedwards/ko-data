e813f603051dc60508616a62c448224b81a4deb6
Cameron Edwards <cameronprattedwards@gmail.com>
2014-07-02 11:08:05 -0400

    In .map, maintain target integrity

--- extenders/map.js
+++ extenders/map.js
@@ -1,28 +1,38 @@
 define(["knockout"], function (ko) {
 	ko.extenders.map = function (target, callback) {
 		var coll = target(),
-			push,
-			unshift,
-			splice;
+			targPush,
+			targUnshift,
+			targSplice,
+			copyPush,
+			copyUnshift,
+			copySplice,
+			copy = ko.observableArray(coll);
 
 		target.splice.apply(target, [0, target.length].concat(coll.map(callback)));
 
-		push = target.push;
+		targPush = target.push;
+		copyPush = copy.push;
 		target.push = function () {
+			targPush.apply(target, arguments);
 			var args = Array.prototype.slice.call(arguments, 0).map(callback);
-			push.apply(target, args);
+			copyPush.apply(copy, args);
 		};
 
-		unshift = target.unshift;
+		targUnshift = target.unshift;
+		copyUnshift = copy.unshift;
 		target.unshift = function () {
+			targUnshift.apply(target, arguments);
 			var args = Array.prototype.slice.call(arguments, 0).map(callback);
-			unshift.apply(target, args);
+			copyUnshift.apply(target, args);
 		};
 
-		splice = target.splice;
+		targSplice = target.splice;
+		copySplice = copy.splice;
 		target.splice = function (index, length, replacements) {
+			targSplice.apply(target, arguments);
 			replacements = Array.prototype.slice.call(arguments, 2).map(callback);
-			splice.apply(target, [index, length].concat(replacements));
+			copySplice.apply(copy, [index, length].concat(replacements));
 		};
 
 		return target;
