0df0ce0b17c55aa109a403709c82229c04812d38
Cameron Edwards <cameronprattedwards@gmail.com>
2014-07-02 10:56:52 -0400

    Add utility .map method for easier model -> viewModel mapping

---
+++ extenders/map.js
@@ -0,0 +1,30 @@
+define(["knockout"], function (ko) {
+	ko.extenders.map = function (target, callback) {
+		var coll = target(),
+			push,
+			unshift,
+			splice;
+
+		target.splice.apply(target, [0, target.length].concat(coll.map(callback)));
+
+		push = target.push;
+		target.push = function () {
+			var args = Array.prototype.slice.call(arguments, 0).map(callback);
+			push.apply(target, args);
+		};
+
+		unshift = target.unshift;
+		target.unshift = function () {
+			var args = Array.prototype.slice.call(arguments, 0).map(callback);
+			unshift.apply(target, args);
+		};
+
+		splice = target.splice;
+		target.splice = function (index, length, replacements) {
+			replacements = Array.prototype.slice.call(arguments, 2).map(callback);
+			splice.apply(target, [index, length].concat(args));
+		};
+
+		return target;
+	}
+});
